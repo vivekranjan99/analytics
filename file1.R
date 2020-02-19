be3edb02d00b34bafc9b12d95411ce986cba0f94
vivekranjan99 <vivekranjan99@gmail.com>
2020-02-19 17:49:30 +0530

    just commit

--- file1.R
+++ file1.R
@@ -1,3 +1,12 @@
 mtcars
 ntcars
 mtcars
+women
+data()
+data
+data()
+AirPassengers
+git config --global user.email "vivekranjan99@gmail.com"
+git config --global user.name "vivekranjan99"
+irish
+iris
